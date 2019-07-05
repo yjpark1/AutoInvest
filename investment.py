3096cef2901cd7058a25c8f695582a4a75f42ac6
yjpark1 <dmqm.yjpark@gmail.com>
2019-07-05 18:42:37 +0900

    some fix

--- investment.py
+++ investment.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
 
+from database import DataBase
+from score import StockScore
+from finditem import EqualWeight, FilterItem
+from portfolio import PortOptim
+from evaluation import Evaluation
+
 
 class Investment:
+    # TODO: check architecture
     def __init__(self, period):
         self.period = period  # unit: month
 
@@ -12,5 +19,12 @@ class Investment:
     def rebalance(self, price):
         return
 
-    def returns_by_period(self, price_start, price_end, weight):
+    def returns_by_quarter(self, price_start, price_end, weight):
+        return
+
+    def run_invest_simulation(self, y_start, y_end):
+
+        for y in range(y_start, y_end):
+            # TODO: loop for quarter
+
         return
